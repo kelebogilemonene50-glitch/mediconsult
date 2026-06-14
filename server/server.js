57e840c7459385e413d5995b272f46a988025092
kelebogilemonene50-glitch <kelebogilemonene50@gmail.com>
2026-06-14 17:06:32 +0200

    Fix page not loading

--- server/server.js
+++ server/server.js
@@ -13,6 +13,14 @@ const io = new Server(server, {
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // ══════════════════════════════════════════
 //  IN-MEMORY STORAGE
 //  (Data lives as long as server is running)
